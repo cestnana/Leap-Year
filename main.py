497fd1cf3369bfdcf349356c5ad1129274f7845a
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-08 04:41:49 +0800

    [Refactor] simplify the code in main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 def is_leap_year(year):
     result = None
     if year % 4 == 0:
-        # continue logic part
         if year % 100 != 0:
-            # continue logic part
             result = True
             return result
         else:
             if year % 400 == 0:
-                # continue logic part
                 result = True
                 return result
             else:
@@ -18,6 +15,4 @@ def is_leap_year(year):
         result = False
         return result
 
-# print(f"Is 2000 Leap Year? {is_leap_year(2000)}.")
-year_to_test = int(input(f"Which year do you want to test? "))
-print(f"Is {year_to_test} Leap Year? {is_leap_year(year_to_test)}.")
+is_leap_year(2024)
